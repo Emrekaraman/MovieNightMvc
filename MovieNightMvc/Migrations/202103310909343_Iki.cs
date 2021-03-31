0381ff525b697f23e8843cfcb30588066750af59
Emre Karaman <karaman.66@hotmail.com>
2021-03-31 13:51:21 +0300

    Add project files.

---
+++ MovieNightMvc/Migrations/202103310909343_Iki.cs
@@ -0,0 +1,22 @@
+namespace MovieNightMvc.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Iki : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Movies", "Actors", c => c.String());
+            AlterColumn("dbo.Movies", "Year", c => c.Int());
+            DropColumn("dbo.Movies", "Actor");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Movies", "Actor", c => c.String());
+            AlterColumn("dbo.Movies", "Year", c => c.Int(nullable: false));
+            DropColumn("dbo.Movies", "Actors");
+        }
+    }
+}
